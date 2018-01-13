0daab03b9323eccd9b4237112056ac74aef37051
Md. Shahadul Alam Patwary <shahadul_alam_17@outlook.com>
2018-01-13 23:33:45 +0600

    Updated

--- main.c
+++ main.c
@@ -1,24 +1,31 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define STRING_LENGTH 50
 #define DICTIONARY_LENGTH 466500
 
 static char query[STRING_LENGTH], dictionary[DICTIONARY_LENGTH][STRING_LENGTH];
-static int tasksCompleted = 0, perThreadWords = 0;
+static int tasksCompleted = 0;
 
 static pthread_mutex_t mutex;
 
-void *taskHandler(void *argument)
+struct Argument
 {
-    int i = 0, startingIndex = (int)argument, lastIndex = startingIndex + perThreadWords;
+    int threadID, startingIndex, lastIndex;
+};
 
-    for (i = startingIndex; i <= lastIndex && i <= DICTIONARY_LENGTH; i++)
+void *taskHandler(void *_argument)
+{
+    int i = 0;
+    struct Argument *argument = (struct Argument *)_argument;
+
+    for (i = argument->startingIndex; i <= argument->lastIndex && i <= DICTIONARY_LENGTH; i++)
     {
         if (strncmp(dictionary[i], query, strlen(query)) == 0)
         {
-            printf("found: %s\n", dictionary[i]);
+            printf("Thread %d: %s\n", argument->threadID, dictionary[i]);
         }
     }
 
@@ -33,18 +40,22 @@ void *taskHandler(void *argument)
 
 int main()
 {
-    int i = 0, threads = 0, startingIndex = 0;
+    int i = 0, threads = 0, perThreadWords = 0, startingIndex = 0;
 
     FILE *file = NULL;
     pthread_t thread;
+
+    struct Argument *arguments;
 
     printf("Enter your query: ");
     scanf("%s", query);
 
     printf("Enter the number of threads: ");
     scanf("%d", &threads);
+    printf("\n");
 
     perThreadWords = (DICTIONARY_LENGTH / threads) + 1;     // calculating how many words each thread will process...
+    arguments = (struct Argument *)malloc(sizeof(struct Argument) * threads);
 
     if ((file = fopen("dictionary.txt", "r")))      // opening dictionary file as "read-only"...
     {
@@ -53,12 +64,16 @@ int main()
             i++;
         }
 
-        fclose(file);   // releasing resource...
-        pthread_mutex_init(&mutex, NULL);
+        fclose(file);       // releasing resource...
+        pthread_mutex_init(&mutex, NULL);       // initializing mutex for critical section...
 
         for (i = 0; i < threads; i++)
         {
-            if (pthread_create(&thread, NULL, taskHandler, (void *)startingIndex) != 0)
+            arguments[i].threadID = i + 1;
+            arguments[i].startingIndex = startingIndex;
+            arguments[i].lastIndex = startingIndex + perThreadWords;
+
+            if (pthread_create(&thread, NULL, taskHandler, &arguments[i]) != 0)
             {
                 printf("error: thread creation failed...\n\n");
             }
@@ -73,13 +88,15 @@ int main()
                 break;
             }
         }
+
+        pthread_mutex_destroy(&mutex);       // releasing resource...
     }
     else
     {
         printf("error: unable to open the dictionary file...\n\n");
     }
 
-    pthread_mutex_destroy(&mutex);
+    free(arguments);        // releasing resource...
 
     return 0;
 }
